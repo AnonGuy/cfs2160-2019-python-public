7e6af3e723c9455d833fb99cb396f4662d6c8dc2
Jeremiah Boby <mail@jeremiahboby.me>
2019-10-10 15:40:15 +0100

    Added labs.py, fixed executable bit

---
+++ 02/labs.py
@@ -0,0 +1,15 @@
+"""Output the number of labs required for a certain number of students."""
+
+students = input('Enter the number of students: ')
+
+while not students.isnumeric():
+    students = input(
+        "Numbers are usually numeric. Let's try this again.\n"
+        'Enter the number of students: '
+    )
+students = int(students)
+
+labs, students = divmod(students, 24)
+labs += bool(students)
+
+print(f'Lab/s required: {labs}')
