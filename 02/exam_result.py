40be8041c6ecb66ceb4bcf4f4a9c663784f800aa
Jeremiah Boby <mail@jeremiahboby.me>
2019-10-10 15:13:38 +0100

    Refactor week 3

--- 02/exam_result.py
+++ 02/exam_result.py
@@ -1,27 +1,18 @@
-#!/usr/bin/env python3
-
 """
-    Process five student marks to find the average.
-
-    Display average along with the student's name.
+Process five student marks to find the average.
+Display average along with the student's name.
 """
 
-__author__  = "Tony Jenkins"
-__email__   = "tony.jenkins@elder-studios.co.uk"
-__date__    = "2018-09-30"
-__license__ = "Unlicense"
+name = input("Enter the student's name: ")
 
-name = input ('Enter the student\'s name: ')
+marks = []
+for exam in range(1, 6):
+    mark = input(f'Enter result {exam}: ')
+    while not mark.isnumeric():
+        mark = input(
+            'Marks should be numeric.\n'
+            f'Enter result {exam}: '
+        )
+    marks.append(int(mark))
 
-mark_1 = int (input ('Enter first result:  '))
-mark_2 = int (input ('Enter second result: '))
-mark_3 = int (input ('Enter third result:  '))
-mark_4 = int (input ('Enter fourth result: '))
-mark_5 = int (input ('Enter fifth result:  '))
-
-total_marks = mark_1 + mark_2 + mark_3 + mark_4 + mark_5
-
-average_mark = total_marks / 5
-
-print ()
-print ('Final Mark for ' + name + ' is ' + str (average_mark) + '.')
+print(f'Final Mark for {name} is {sum(marks) / len(marks)}')
